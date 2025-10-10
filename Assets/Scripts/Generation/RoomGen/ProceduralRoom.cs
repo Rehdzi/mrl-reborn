5309dad05212ba51a1600113290af844ad453553
Rehdzi <5crkurwa@gmail.com>
2025-10-10 16:19:08 +0300

    Removing walls inside of rooms

--- Assets/Scripts/Generation/RoomGen/ProceduralRoom.cs
+++ Assets/Scripts/Generation/RoomGen/ProceduralRoom.cs
@@ -4,15 +4,12 @@ namespace Generation.RoomGen
 {
     public class WFCRoom : MonoBehaviour
     {
-
         public GameObject floorPrefab;
         public GameObject wallPrefab;
-    
         public Vector2Int roomSize;
 
         private int numberOfFloors = 1;
         private float cellUnitSize;
-
         private Floor[] floors;
         
         void GenerateRoom()
@@ -44,7 +41,6 @@ namespace Generation.RoomGen
 
                 floors[floorCount] = new Floor(floorCount++, rooms);
             }
-
         }
 
         void RenderRoom()
@@ -57,50 +53,63 @@ namespace Generation.RoomGen
                     {
                         RectangleRoom room = floor.rooms[x, y];
 
+                        // Создаем пол для каждого тайла
                         var floorMesh = Instantiate(
                             floorPrefab,
                             new Vector3(room.RoomPosition.x, floor.FloorNumber, room.RoomPosition.y),
                             Quaternion.Euler(Vector3.zero));
                         floorMesh.transform.parent = transform;
                         
-                        var wall1 = Instantiate(
-                            wallPrefab, 
-                            new Vector3(room.RoomPosition.x, floor.FloorNumber + 0.5f, room.RoomPosition.y + 0.5f), 
-                            Quaternion.Euler(0,0,0));
-                        wall1.transform.parent = transform;
-                        var wall2 = Instantiate(
-                            wallPrefab, 
-                            new Vector3(room.RoomPosition.x + 0.5f, floor.FloorNumber + 0.5f, room.RoomPosition.y), 
-                            Quaternion.Euler(0,90,0));
-                        wall2.transform.parent = transform;
-                        var wall3 = Instantiate(
-                            wallPrefab, 
-                            new Vector3(room.RoomPosition.x, floor.FloorNumber + 0.5f, room.RoomPosition.y - 0.5f), 
-                            Quaternion.Euler(0,180,0));
-                        wall3.transform.parent = transform;
-                        var wall4 = Instantiate(
-                            wallPrefab, 
-                            new Vector3(room.RoomPosition.x - 0.5f, floor.FloorNumber + 0.5f, room.RoomPosition.y), 
-                            Quaternion.Euler(0,-90,0));
-                        wall4.transform.parent = transform;
+                        // Создаем стены только по границам комнаты
+                        if (y == roomSize.y - 1) // Верхняя стена (Z+)
+                        {
+                            var wall1 = Instantiate(
+                                wallPrefab, 
+                                new Vector3(room.RoomPosition.x, floor.FloorNumber + 0.5f, room.RoomPosition.y + 0.5f), 
+                                Quaternion.Euler(0,0,0));
+                            wall1.transform.parent = transform;
+                        }
                         
+                        if (x == roomSize.x - 1) // Правая стена (X+)
+                        {
+                            var wall2 = Instantiate(
+                                wallPrefab, 
+                                new Vector3(room.RoomPosition.x + 0.5f, floor.FloorNumber + 0.5f, room.RoomPosition.y), 
+                                Quaternion.Euler(0,90,0));
+                            wall2.transform.parent = transform;
+                        }
+                        
+                        if (y == 0) // Нижняя стена (Z-)
+                        {
+                            var wall3 = Instantiate(
+                                wallPrefab, 
+                                new Vector3(room.RoomPosition.x, floor.FloorNumber + 0.5f, room.RoomPosition.y - 0.5f), 
+                                Quaternion.Euler(0,180,0));
+                            wall3.transform.parent = transform;
+                        }
+                        
+                        if (x == 0) // Левая стена (X-)
+                        {
+                            var wall4 = Instantiate(
+                                wallPrefab, 
+                                new Vector3(room.RoomPosition.x - 0.5f, floor.FloorNumber + 0.5f, room.RoomPosition.y), 
+                                Quaternion.Euler(0,-90,0));
+                            wall4.transform.parent = transform;
+                        }
                     }
                 }
             }
         }
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
+
         void Start()
         {
             GenerateRoom();
             RenderRoom();
         }
 
-        // Update is called once per frame
         void Update()
         {
         
         }
     }
 }
-
-
