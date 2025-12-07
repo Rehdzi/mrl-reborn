137142d025334ff8ad1f290719bce78db5b43b15
Rehdzi <5crkurwa@gmail.com>
2025-12-07 05:21:04 +0300

    Attack state finalize

--- Assets/Resources/Animations/Scripts/EntityAttackState.cs
+++ Assets/Resources/Animations/Scripts/EntityAttackState.cs
@@ -1,3 +1,4 @@
+using Entities;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -13,12 +14,36 @@ public class EntityAttackState : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent = animator.GetComponent<NavMeshAgent>();
+        attackController = animator.GetComponent<AttackController>();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (attackController.targetToAttack != null && 
+            animator.transform.GetComponent<EntityMovement>().isCommandedToMove == false)
+        {
+            LookAtTarget();
+            agent.SetDestination(attackController.targetToAttack.position);
+            
+            float distanceFromTarget = Vector3.Distance(attackController.targetToAttack.position, animator.transform.position);
+            if (distanceFromTarget > stopAttackingDistance || 
+                attackController.targetToAttack == null)
+            {
+                agent.SetDestination(animator.transform.position);
+                animator.SetBool("IsAttack", false);
+            }
+        }
+    }
+
+    private void LookAtTarget()
+    {
+        Vector3 direction = attackController.targetToAttack.position - agent.transform.position;
+        agent.transform.rotation = Quaternion.LookRotation(direction);
+        
+        var yRotation = agent.transform.eulerAngles.y;
+        agent.transform.rotation = Quaternion.Euler(0, yRotation, 0);
         
     }
 
